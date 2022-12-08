c44299231bc7c969a14059ed34876d12ce838900
Shubham Agarwal <shagarw@adobe.com>
2022-12-08 17:43:55 +0530

    Updates

---
+++ precache-manifest.d22c44b8bc72946233b2e4e0eb7be4ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65b7ccfa1044704c44d8de11a2b379f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c54acca2d92b875ad57",
+    "url": "/static/css/main.7841b658.chunk.css"
+  },
+  {
+    "revision": "5de8c52d36397cb40f29",
+    "url": "/static/js/2.cd289270.chunk.js"
+  },
+  {
+    "revision": "cf4263b26236207b597a32c399243e67",
+    "url": "/static/js/2.cd289270.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c54acca2d92b875ad57",
+    "url": "/static/js/main.2cd362e6.chunk.js"
+  },
+  {
+    "revision": "45d009624ee73ac83b54",
+    "url": "/static/js/runtime-main.cdd681a7.js"
+  }
+]);
